730c3b4b0f3a9efd35f03abd32ec29c6c0490e7c
Estabillo, Ivy Lesline <ivy.l.estabillio@accenture.com>
2021-09-30 21:41:49 +0800

    set up for heroku

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,21 +8,22 @@ const forecast = (latitude, longitude, callback) => {
     longitude +
     "&units=f";
 
-  request({ url: url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback("Unable to connect to weather service!", undefined);
-    } else if (response.body.error) {
-      callback("Unable to find location", undefined);
+    } else if (body.error) {
+      callback("Unable to find location. Try another search.", undefined);
     } else {
       callback(
         undefined,
-        response.body.current.weather_descriptions[0] +
+        body.current.weather_descriptions[0] +
           ". It is currently " +
-          response.body.current.temperature +
-          " degress out."
+          body.current.temperature +
+          " degress out. It feels like " +
+          body.current.feelslike +
+          " degrees out."
       );
     }
   });
 };
-
 module.exports = forecast;
