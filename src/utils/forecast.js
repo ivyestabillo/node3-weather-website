d018d656759f56df44ee372e313fd8540b92d7dc
Estabillo, Ivy Lesline <ivy.l.estabillio@accenture.com>
2021-09-30 15:17:15 +0800

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,28 @@
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    "http://api.weatherstack.com/current?access_key=506b19ed32b7510b8367a864d0374b8a&query=" +
+    latitude +
+    "," +
+    longitude +
+    "&units=f";
+
+  request({ url: url, json: true }, (error, response) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (response.body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        response.body.current.weather_descriptions[0] +
+          ". It is currently " +
+          response.body.current.temperature +
+          " degress out."
+      );
+    }
+  });
+};
+
+module.exports = forecast;
